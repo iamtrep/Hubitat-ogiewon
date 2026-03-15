5a8628bdbda95d6e168626c9a4e1c8c0f90fdca3
PJ <120332908+iamtrep@users.noreply.github.com>
2026-03-15 08:02:44 -0400

    Update aqara-fp300.groovy

--- Drivers/aqara-fp300.src/aqara-fp300.groovy
+++ Drivers/aqara-fp300.src/aqara-fp300.groovy
@@ -195,7 +195,11 @@ private void parseAttribute(String description, Map descMap, Map it) {
             break
         case "0000":
             if (it.attrId == "0001") sendRttEvent()
-            else if (it.attrId == "0005") sendInfoEvent("Button was pressed – device awake for 15 min")
+            else if (it.attrId == "0005") {
+                if (it.value && it.value.length() > 2) { device.updateDataValue("model", it.value ?: ""); logDebug "Model: ${it.value}" }
+                else sendInfoEvent("Button was pressed – device awake for 15 min")
+            }
+            else if (it.attrId == "0006") { device.updateDataValue("dateCode", it.value ?: ""); logDebug "Date code: ${it.value}" }
             else if (it.attrId == "FF01") parseAqaraAttributeFF01(description)
             break
         case "FCC0":
@@ -456,11 +460,34 @@ void decodeAqaraStruct(String description) {
                 rawValue = Integer.parseInt(valueHex[(i+6)..(i+7)] + valueHex[(i+4)..(i+5)], 16)
                 switch (tag) {
                     case 0x01: voltageAndBatteryEvents(rawValue / 1000.0); break
+                    case 0x05: device.updateDataValue("aqaraTag05", rawValue.toString()); logDebug "Aqara struct tag 0x05: ${rawValue}"; break
+                    case 0x0A: device.updateDataValue("aqaraTag0A", rawValue.toString()); logDebug "Aqara struct tag 0x0A: ${rawValue}"; break
                     case 0x17: sendVoltageEvent(rawValue / 1000.0); break
                     default:   logDebug "decodeAqaraStruct 2B unhandled tag=0x${valueHex[(i+0)..(i+1)]} val=${rawValue}"
                 }
                 i += 8; break
+            case 0x42:  // String type – Aqara often encodes firmware version here
+                int strLen = Integer.parseInt(valueHex[(i+4)..(i+5)], 16)
+                if (i + 6 + strLen * 2 <= MsgLength) {
+                    String strVal = new String(valueHex[(i+6)..(i+5+strLen*2)].decodeHex())
+                    switch (tag) {
+                        case 0x03: device.updateDataValue("aqaraFirmware", strVal); logDebug "Aqara firmware (tag 0x03): ${strVal}"; break
+                        case 0x05: device.updateDataValue("aqaraModel", strVal); logDebug "Aqara model (tag 0x05): ${strVal}"; break
+                        default:   logDebug "decodeAqaraStruct string tag=0x${valueHex[(i+0)..(i+1)]} val='${strVal}'"
+                    }
+                }
+                i += 6 + strLen * 2; break
             case 0x0B: case 0x1B: case 0x23: case 0x2B:
+                if (dataType == 0x23) {
+                    rawValue = Integer.parseInt(valueHex[(i+10)..(i+11)] + valueHex[(i+8)..(i+9)] + valueHex[(i+6)..(i+7)] + valueHex[(i+4)..(i+5)], 16)
+                    if (tag == 0x0D) {
+                        String fwVer = "${(rawValue >> 24) & 0xFF}.${(rawValue >> 16) & 0xFF}.${rawValue & 0xFFFF}"
+                        device.updateDataValue("aqaraVersion", fwVer)
+                        logDebug "Aqara firmware version (tag 0x0D): ${fwVer}"
+                    } else {
+                        logDebug "decodeAqaraStruct 4B tag=0x${valueHex[(i+0)..(i+1)]} val=${rawValue}"
+                    }
+                }
                 i += 12; break
             case 0x24:
                 i += 14; break
@@ -655,6 +682,10 @@ void refresh() {
     cmds += zigbee.readAttribute(0x0402, 0x0000, [:], delay=200)
     cmds += zigbee.readAttribute(0x0405, 0x0000, [:], delay=200)
     cmds += zigbee.readAttribute(0x0400, 0x0000, [:], delay=200)
+    // Basic cluster – device details
+    cmds += zigbee.readAttribute(zigbee.BASIC_CLUSTER, [0x0004, 0x0005, 0x0006], [:], delay=200)
+    // Aqara proprietary struct – often contains firmware version and model
+    cmds += zigbee.readAttribute(0xFCC0, 0x00F7, [mfgCode: 0x115F], delay=200)
     sendZigbeeCommands(cmds)
 }
 
